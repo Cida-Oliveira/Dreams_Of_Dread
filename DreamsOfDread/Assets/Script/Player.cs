09620862c702361f0e753a41cbec1f9c6aacc74a
Cida-Oliveira <145566499+Cida-Oliveira@users.noreply.github.com>
2025-01-28 16:51:39 -0300

    Cenario e Script do player

---
+++ DreamsOfDread/Assets/Script/Player.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plyer : MonoBehaviour
+{
+    public float Speed;
+    public float jumpForce;
+
+    public bool isJump;
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movimento * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            //eulerAngles serve para a rotação
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(isJump == false)
+            {
+                rig.AddForce(new Vector3(0f, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector3(0f, jumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if(collision.gameObject.tag){}
+
+        if(collision.gameObject.tag == "Over")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
