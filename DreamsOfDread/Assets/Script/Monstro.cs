d437c98228c6dd7ac511dc4c119607f1cfa77560
Cida-Oliveira <145566499+Cida-Oliveira@users.noreply.github.com>
2025-02-03 08:35:44 -0300

    monstro
    inicio da movimentação do monstro

---
+++ DreamsOfDread/Assets/Script/Monstro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monstro : MonoBehaviour
+{
+    public float vel = 2f;
+    public Transform destino;
+
+    public bool chegou = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
